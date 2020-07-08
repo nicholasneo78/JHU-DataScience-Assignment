73ad1c7109601f5659bbecc64a30b1811e43c1d2
Nicholas Neo <nicholasneo78@gmail.com>
2020-07-08 14:00:22 +0800

    added assignment 5.2

--- Course 4.4/plot6.R
+++ Course 4.4/plot6.R
@@ -51,3 +51,8 @@ ggplot(totalemissions, aes(factor(year), emissions, fill = location)) +
   ylim(range(pretty(c(0, totalemissions$emissions)))) + 
   theme_bw()
 dev.off()
+
+# QUESTION 6
+#'Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los #'Angeles County, 
+#'California (fips == "06037"). Which city has seen greater changes 
+#'over time in motor vehicle emissions?
